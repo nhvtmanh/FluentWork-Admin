c5ad933584c2c6febf951e05f61d6ca3c80b59ef
nhvtmanh <nhvtmanh@gmail.com>
2025-04-14 23:54:03 +0700

    add Login page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FluentWork_Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
