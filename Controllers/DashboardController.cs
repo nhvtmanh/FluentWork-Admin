8090909fc2d4ea81b7b1f85a703d1ab0e6101020
nhvtmanh <nhvtmanh@gmail.com>
2025-04-24 22:48:57 +0700

    add routing

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ namespace FluentWork_Admin.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["ActiveMenu"] = "Dashboard";
             return View();
         }
     }
--- Program.cs
+++ Program.cs
@@ -24,4 +24,49 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Account}/{action=Login}/{id?}");
 
+app.MapControllerRoute(
+    name: "Admin login",
+    pattern: "admin/login",
+    defaults: new { controller = "Account", action = "Login" });
+
+app.MapControllerRoute(
+    name: "Admin register",
+    pattern: "admin/register",
+    defaults: new { controller = "Account", action = "Register" });
+
+app.MapControllerRoute(
+    name: "Admin forgot password",
+    pattern: "admin/forgot-password",
+    defaults: new { controller = "Account", action = "ForgotPassword" });
+
+app.MapControllerRoute(
+    name: "Dashboard",
+    pattern: "admin/dashboard",
+    defaults: new { controller = "Dashboard", action = "Index" });
+
+app.MapControllerRoute(
+    name: "Manage lessons",
+    pattern: "admin/manage-lessons",
+    defaults: new { controller = "Lesson", action = "Index" });
+
+app.MapControllerRoute(
+    name: "Manage questions",
+    pattern: "admin/manage-questions",
+    defaults: new { controller = "Question", action = "Index" });
+
+app.MapControllerRoute(
+    name: "Manage tests",
+    pattern: "admin/manage-tests",
+    defaults: new { controller = "Test", action = "Index" });
+
+app.MapControllerRoute(
+    name: "Manage flashcards",
+    pattern: "admin/manage-flashcards",
+    defaults: new { controller = "Flashcard", action = "Index" });
+
+app.MapControllerRoute(
+    name: "Manage users",
+    pattern: "admin/manage-users",
+    defaults: new { controller = "User", action = "Index" });
+
 app.Run();
